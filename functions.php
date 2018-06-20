de535790f068be27456aa4a45ce8b5331dd00d83
liamdigitalstorm <34305552+liamdigitalstorm@users.noreply.github.com>
2018-06-20 17:18:40 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+//Appearance Menu
+//SITE LOGO
+add_theme_support( 'custom-logo' );
+
+function dstheme_custom_logo_setup() {
+    $defaults = array(
+        'height'      => 100,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ),
+    );
+    add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'dstheme_custom_logo_setup' );
